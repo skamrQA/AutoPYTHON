62fa2c9d3a2eedec4eb15e72b184d4fe99809022
youtried <59833201+youtried@users.noreply.github.com>
2024-04-30 14:50:46 +0300

    full__HW_PY4

--- Lesson4/test_string_utils.py
+++ Lesson4/test_string_utils.py
@@ -6,7 +6,7 @@ stringutils = StringUtils()
 
 # метод capitilize
 
-@pytest.mark.test_capitalize_positive
+@pytest.mark.capitalize_positive
 @pytest.mark.parametrize( 'str, result', [("hihi", "Hihi"),
                                            ("123", "123"),
                                            ("good", "Good")] )
@@ -16,7 +16,7 @@ def test_capitalize_positive(str, result):
     assert res == result
 
 @pytest.mark.xfail
-@pytest.mark.test_capitalize_negative
+@pytest.mark.capitalize_negative
 @pytest.mark.parametrize( 'str, result', [("", ""),
                                           (" ", " "),
                                           (None, None),
@@ -29,7 +29,7 @@ def test_capitalize_negative(str, result):
 
 # метод trim
 
-@pytest.mark.test_trim_positive
+@pytest.mark.trim_positive
 @pytest.mark.parametrize( 'str, result', [("   Clow", "Clow"),
                                            ("   123", "123"),
                                            ("   .", ".")] )
@@ -39,7 +39,7 @@ def test_trim_positive(str, result):
     assert res == result
 
 @pytest.mark.xfail
-@pytest.mark.test_trim_negative
+@pytest.mark.trim_negative
 @pytest.mark.parametrize( 'str, result', [(   123, 123),
                                           (" S u n", "Sun"),
                                           (" G ood", "Good"),
@@ -52,7 +52,7 @@ def test_trim_negative(str, result):
 
 # метод to_list
     
-@pytest.mark.test_to_list_positive
+@pytest.mark.to_list_positive
 @pytest.mark.parametrize( 'str, delim, result', [("1,2,3", ",", ["1", "2", "3"])])
 def test_to_list_positive(str, delim, result):
     stringutils = StringUtils()
@@ -60,7 +60,7 @@ def test_to_list_positive(str, delim, result):
     assert res == result
 
 @pytest.mark.xfail
-@pytest.mark.test_to_list_negative
+@pytest.mark.to_list_negative
 @pytest.mark.parametrize( 'str, delim, result', [(" , , ", ",", [" ", " ", " "])] )
 def test_to_list_negative(str, delim, result):
     stringutils = StringUtils()
@@ -69,7 +69,7 @@ def test_to_list_negative(str, delim, result):
 
 # метод contains
     
-@pytest.mark.test_contains_positive
+@pytest.mark.contains_positive
 @pytest.mark.parametrize( 'str, symbol, result', [("1,2,3", "1", True),
                                                   ("qwerty", "a", False)])
 def test_contains_positive(str, symbol, result):
@@ -78,7 +78,7 @@ def test_contains_positive(str, symbol, result):
     assert res == result
 
 @pytest.mark.xfail
-@pytest.mark.test_contains_negative
+@pytest.mark.contains_negative
 @pytest.mark.parametrize( 'str, symbol, result', [("1,2,3", "5", True)])
 def test_contains_negative(str, symbol, result):
     stringutils = StringUtils()
@@ -96,7 +96,7 @@ def test_delete_symbol_positive(str, symbol, result):
     assert res == result
 
 @pytest.mark.xfail
-@pytest.mark.test_delete_symbol_negative
+@pytest.mark.delete_symbol_negative
 @pytest.mark.parametrize( 'str, symbol, result', [("1,2,3", "5", "1,2,3")])
 def test_delete_symbol_negative(str, symbol, result):
     stringutils = StringUtils()
@@ -115,7 +115,7 @@ def test_starts_with_positive(str, symbol, result):
     assert res == result
 
 @pytest.mark.xfail
-@pytest.mark.test_starts_with_negative
+@pytest.mark.starts_with_negative
 @pytest.mark.parametrize( 'str, symbol, result', [("1,2,3", None, True)])
 def test_starts_with_negative(str, symbol, result):
     stringutils = StringUtils()
@@ -134,7 +134,7 @@ def test_end_with_positive(str, symbol, result):
     assert res == result
 
 @pytest.mark.xfail
-@pytest.mark.test_end_with_negative
+@pytest.mark.end_with_negative
 @pytest.mark.parametrize( 'str, symbol, result', [("1,2,3", None, True),
                                                   ("", "", True)])
 def test_end_with_negative(str, symbol, result):
@@ -154,7 +154,7 @@ def test_is_empty_positive(str, result):
     assert res == result
 
 @pytest.mark.xfail
-@pytest.mark.test_is_empty_negative
+@pytest.mark.is_empty_negative
 @pytest.mark.parametrize( 'str, result', [("     ",  True),
                                           (None, True),
                                           ])
@@ -175,7 +175,7 @@ def test_list_to_string_positive(lst, joi, result):
     assert res == result
 
 @pytest.mark.xfail
-@pytest.mark.test_list_to_string_negative
+@pytest.mark.list_to_string_negative
 @pytest.mark.parametrize( 'lst, joi, result', [([" , , ,"], "*", "* * *" ),
                                                ([None], "!", ""),
                                               ])
