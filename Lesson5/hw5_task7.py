ebed034f06c536639342a8d3882be194f7241a46
youtried <59833201+youtried@users.noreply.github.com>
2024-05-02 22:34:45 +0300

    full_HW_PY5

---
+++ Lesson5/hw5_task7.py
@@ -0,0 +1,22 @@
+#открыть страничку в Chrome 
+from selenium import webdriver 
+from time import sleep
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome()
+
+# открыть ссылку
+driver.get("http://the-internet.herokuapp.com/inputs")
+Element = 'input'
+driver.findelement(By.CSSSELECTOR, Element).click()
+click.send_keys("1000")
+sleep(2)
+click.clear()
+sleep(2)
+
+# Буквы не впишутся так как поле не для букв
+click.send_keys("999") 
+
+sleep(2)
+driver.quit()
