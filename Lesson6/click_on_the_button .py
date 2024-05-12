8d79659eb25792e39c510e7bed521ff58a7e7599
youtried <59833201+youtried@users.noreply.github.com>
2024-05-12 18:22:56 +0300

    Revert "full_HW___PY6"
    This reverts commit b301f73271c7b87e952aef39fa42d83051c13024.

--- Lesson6/click_on_the_button .py
+++ Lesson6/click_on_the_button .py
@@ -1,19 +1,19 @@
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+# from time import sleep
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service as ChromeService
+# from selenium.webdriver.common.by import By 
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+# driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver.get("http://uitestingplayground.com/ajax")
-driver.find_element(By.CSS_SELECTOR, '#ajaxButton').click()
+# driver.get("http://uitestingplayground.com/ajax")
+# driver.find_element(By.CSS_SELECTOR, '#ajaxButton').click()
 
-green_banner = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "bg-success"))
-    )
-text = green_banner.text
-print(text)
-driver.quit()
+# green_banner = WebDriverWait(driver, 20).until(
+#         EC.presence_of_element_located((By.CLASS_NAME, "bg-success"))
+#     )
+# text = green_banner.text
+# print(text)
+# driver.quit()
--- Lesson6/rename_the_button.py
+++ Lesson6/rename_the_button.py
@@ -1,17 +1,17 @@
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By 
+# from time import sleep
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service as ChromeService
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.common.by import By 
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.get("http://uitestingplayground.com/textinput")
+# driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+# driver.get("http://uitestingplayground.com/textinput")
 
-element = driver.find_element(By.CSS_SELECTOR, "#newButtonName") 
-element.send_keys("Skypro")
-driver.find_element(By.CSS_SELECTOR, "button[id=updatingButton]").click()
-button = driver.find_element(By.CSS_SELECTOR, "button[id=updatingButton]")
-txt = button.text
-print(txt) 
+# element = driver.find_element(By.CSS_SELECTOR, "#newButtonName") 
+# element.send_keys("Skypro")
+# driver.find_element(By.CSS_SELECTOR, "button[id=updatingButton]").click()
+# button = driver.find_element(By.CSS_SELECTOR, "button[id=updatingButton]")
+# txt = button.text
+# print(txt) 
 
-driver.quit() 
+# driver.quit() 
