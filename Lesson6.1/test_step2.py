1df5f257129ef04a148280a70d540aa1b07a519d
youtried <servic69@yandex.ru>
2024-05-21 10:04:17 +0300

    full_HW_PY_6.1

--- Lesson6.1/test_step2.py
+++ Lesson6.1/test_step2.py
@@ -1,10 +1,8 @@
-from time import sleep
 import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 @pytest.fixture
 def browser():
